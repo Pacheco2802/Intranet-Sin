4c137a3c520e03dfe64112958674a619c583ba3b
Pacheco Pedro <pedro.pacheco.ph1@gmail.com>
2026-05-21 16:30:22 -0300

    ensure_superuser: sempre atualiza senha e exibe output detalhado

--- core/management/commands/ensure_superuser.py
+++ core/management/commands/ensure_superuser.py
@@ -3,25 +3,36 @@ from decouple import config
 
 
 class Command(BaseCommand):
-    help = 'Cria superusuário a partir de variáveis de ambiente se não existir'
+    help = 'Cria ou atualiza superusuário a partir de variáveis de ambiente'
 
     def handle(self, *args, **options):
         from core.models import CustomUser
         email = config('DJANGO_SUPERUSER_EMAIL', default='')
         password = config('DJANGO_SUPERUSER_PASSWORD', default='')
+
+        self.stdout.write(f'[ensure_superuser] email={email!r} password_set={bool(password)}')
+
         if not email or not password:
-            self.stdout.write('DJANGO_SUPERUSER_EMAIL ou DJANGO_SUPERUSER_PASSWORD não definidos, pulando.')
+            self.stdout.write(self.style.WARNING(
+                '[ensure_superuser] DJANGO_SUPERUSER_EMAIL ou DJANGO_SUPERUSER_PASSWORD não definidos, pulando.'
+            ))
             return
-        if CustomUser.objects.filter(email=email).exists():
-            self.stdout.write(f'Superusuário {email} já existe.')
-            return
-        user = CustomUser.objects.create_superuser(
-            username=CustomUser.generate_username(email),
-            email=email,
-            password=password,
+
+        user, created = CustomUser.objects.get_or_create(
+            email__iexact=email,
+            defaults={'username': CustomUser.generate_username(email), 'email': email},
         )
-        user.role = CustomUser.Role.ADMIN_TI
+
+        user.set_password(password)
+        user.is_active = True
+        user.is_staff = True
+        user.is_superuser = True
         user.is_approved = True
         user.lgpd_consent = True
+        user.role = CustomUser.Role.ADMIN_TI
         user.save()
-        self.stdout.write(self.style.SUCCESS(f'Superusuário {email} criado com sucesso.'))
+
+        if created:
+            self.stdout.write(self.style.SUCCESS(f'[ensure_superuser] Superusuário {email} criado com sucesso.'))
+        else:
+            self.stdout.write(self.style.SUCCESS(f'[ensure_superuser] Superusuário {email} atualizado (senha redefinida).'))
